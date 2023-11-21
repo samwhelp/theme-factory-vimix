4b079f98d5ed392622e13198c1d02d838cddeff5
samwhelp <samwhelp@users.noreply.github.com>
2023-11-21 10:31:09 +0800

    fix

---
+++ .ctrl/clone.sh
@@ -0,0 +1,31 @@
+
+
+
+
+git clone --recurse-submodules -b main git@github.com:samwhelp/theme-factory-vimix.git theme-factory-vimix
+
+cd theme-factory-vimix
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "${OLDPWD}"
+
+
+
+
+
+
+
+
+git clone -b gh-pages git@github.com:samwhelp/theme-factory-vimix.git gh-pages
+
+cd gh-pages
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "${OLDPWD}"
+
+
+
