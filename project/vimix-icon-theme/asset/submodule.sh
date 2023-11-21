ee5caa0c6f2d0b84a55b484cc670e89a2bfce904
samwhelp <samwhelp@users.noreply.github.com>
2023-11-21 10:19:04 +0800

    fix

---
+++ project/vimix-icon-theme/asset/submodule.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git submodule add https://github.com/vinceliuice/vimix-icon-theme.git vimix-icon-theme
---
+++ project/vimix-kde-theme/asset/submodule.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git submodule add https://github.com/vinceliuice/vimix-kde.git vimix-kde-theme
