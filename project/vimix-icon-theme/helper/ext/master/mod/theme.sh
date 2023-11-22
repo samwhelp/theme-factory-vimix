28ce14bbbb22b09c5d19e202cb16ccc6d794d8bd
samwhelp <samwhelp@users.noreply.github.com>
2023-11-22 22:58:02 +0800

    fix

--- project/vimix-icon-theme/helper/ext/master/mod/theme.sh
+++ project/vimix-icon-theme/helper/ext/master/mod/theme.sh
@@ -97,7 +97,7 @@ mod_theme_build_core () {
 	local target_theme_name="${real_theme_name}"
 	local target_gtk_theme_name="${target_theme_name}"
 	local target_metacity_theme_name="${target_theme_name}"
-	local target_icon_theme_name="${real_theme_main_name}${append_theme_bright_name}"
+	local target_icon_theme_name="${real_theme_main_name}${append_theme_color_name}${append_theme_bright_name}"
 	local target_cursor_theme_name="Vimix-Cursor"
 
 
